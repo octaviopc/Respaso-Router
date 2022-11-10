aba301cf02d993b2a65582125c001a744db39a41
octaviopc <operezcenteno@deloitte.com>
2022-11-09 23:14:57 -0600

    We add pages folder and declare it in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import 
+import Welcome from "./Pages/Welcome";
+import Products from "./Pages/Products";
 
 function App() {
   return (
-  <div>
-    <Route path="/welcome">
-
-    </Route>
-  </div>
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
   );
 }
 
 export default App;
 
-//our-domain.com/welcome
+//our-domain.com/welcome => Welcome Component
+//our-domain.com/products=>Products Component
